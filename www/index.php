af50dd87d648c96edd5927f14842d73f1354c87a
martin <m.himlar@gmail.com>
2017-02-14 16:48:53 +0100

    add maintenance.php to index

--- www/index.php
+++ www/index.php
@@ -1,4 +1,6 @@
 <?php
+//require_once('.maintenance.php');
+
 const IMAGES_DIR = __DIR__ . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "gallery" . DIRECTORY_SEPARATOR;
 
 $container = require __DIR__ . '/../app/bootstrap.php';
